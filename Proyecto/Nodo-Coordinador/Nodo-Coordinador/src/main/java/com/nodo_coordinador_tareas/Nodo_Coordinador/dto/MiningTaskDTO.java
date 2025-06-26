3e078328200db87b74b3b4372dd511736f6282d0
rocco <roccobuzzomarcelo@gmail.com>
2025-06-26 17:52:02 -0300

    MiningTaskDTO modificados.

--- Proyecto/Nodo-Coordinador/Nodo-Coordinador/src/main/java/com/nodo_coordinador_tareas/Nodo_Coordinador/dto/MiningTaskDTO.java
+++ Proyecto/Nodo-Coordinador/Nodo-Coordinador/src/main/java/com/nodo_coordinador_tareas/Nodo_Coordinador/dto/MiningTaskDTO.java
@@ -1,17 +1,22 @@
 package com.nodo_coordinador_tareas.Nodo_Coordinador.dto;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.nodo_coordinador_tareas.Nodo_Coordinador.model.Transaction;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@type")
+@JsonTypeName("MiningTask")
 public class MiningTaskDTO {
-    private String blockId;              // 🔐 ID único del BLOQUE CANDIDATO
+    private String blockId;
     private String previousHash;
     private List<Transaction> transactions;
     private int difficulty;
--- Proyecto/Worker/Worker/src/main/java/com/worker/Worker/dto/MiningTaskDTO.java
+++ Proyecto/Worker/Worker/src/main/java/com/worker/Worker/dto/MiningTaskDTO.java
@@ -1,19 +1,26 @@
 package com.worker.Worker.dto;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.worker.Worker.model.Transaction;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Setter
-@Getter
+@Data
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@type")
+@JsonTypeName("MiningTask")
+@NoArgsConstructor
+@AllArgsConstructor
 public class MiningTaskDTO {
-    private String blockId;              // 🔐 ID único del bloque
+    private String blockId;
     private String previousHash;
     private List<Transaction> transactions;
     private int difficulty;
     private long minNonce;
     private long maxNonce;
 }
+
 
